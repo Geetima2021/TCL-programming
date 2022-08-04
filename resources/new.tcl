6be3968d301d48c64c99eb9e8482c29f8d64b44e
Geetima Kachari <er.geetimakachari@gmail.com>
2022-08-04 14:49:25 +0530

    Add files via upload

---
+++ resources/new.tcl
@@ -0,0 +1,51 @@
+#! /bin/env tclsh
+
+set new [glob Post_synthesis/*.rpt]
+
+foreach report $new {
+	set rd_report [open $report r]
+	set filename [file rootname [file tail $report] ]
+        puts "$filename"
+	set pattern {slack}
+        while {[gets $rd_report line] != -1 } {
+		if {[regexp $pattern $line match]} {
+		#set filename  [file tail $report]
+		#set slack [string map {"            " ""}  $line]
+		set slack [lindex $line 0]
+          	#puts "Hold/Setup $pattern value for $filename is $slack"
+		puts "$slack"
+		}
+	}
+close $rd_report
+}
+	              		
+foreach report $new {
+        set rd_report [open $report r]
+	set filename [file rootname [file tail $report] ]
+        puts "$filename"
+        set pattern {Startpoint:}
+    	while {[gets $rd_report line] >= 0 } {
+		if {[regexp $pattern $line]} {
+                set startpoint [lindex $line 1]
+                #puts "$pattern value for $filename is $startpoint"
+	        puts "$startpoint"
+                } 
+         }
+close $rd_report	 
+}
+
+foreach report $new {
+        set rd_report [open $report r]
+	set filename [file rootname [file tail $report] ]
+        puts "$filename"
+        set pattern {Endpoint:}
+	while {[gets $rd_report line] >= 0 } {
+		 if {[regexp $pattern $line match]} {
+                 set filename  [file tail $report]
+                 set endpoint [lindex $line 1]
+                 #puts "$pattern value for $filename is $endpoint"
+		 puts "$endpoint"
+                }
+        }
+close $rd_report
+}
---
+++ resources/rename.tcl
@@ -0,0 +1,29 @@
+#! /bin/env tclsh
+
+set lib_dir [glob ../../../timing_libs/*.lib]
+
+foreach line $lib_dir {
+	
+	set filename [file tail $line ]
+	set name1 [lindex [split [file rootname $filename] hd] 3]
+	
+	set ext [file extension $filename]
+	#set a [file rootname [file tail $line]] 
+	#set c [lindex [split $name hd] 3]
+	set map1 [string map {"__" ""} $name1]
+	set map2  [string map {"_" ""} $map1]
+	
+	set new_name "$map2"
+	set name2 [string map {".lib" "_Post_synthesis_minmax"} $ext]
+
+	read_liberty $line
+	
+	read_verilog rvmyth.synthesis.v
+	link_design rvmyth
+	read_sdc -echo rvmyth.sdc
+	mkdir -p Post_synthesis
+
+	report_checks -path_delay min_max -fields {nets cap slew input_pins} -format full_clock_expanded -digits {4} > Post_synthesis/$new_name$name2.rpt
+
+}
+
