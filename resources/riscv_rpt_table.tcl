744400a6bc398c4880736000595711a7e604f615
Geetima Kachari <er.geetimakachari@gmail.com>
2022-08-21 17:05:20 +0530

    Update riscv_rpt_table.tcl

--- resources/riscv_rpt_table.tcl
+++ resources/riscv_rpt_table.tcl
@@ -3,7 +3,7 @@ set report [open build_report.html w+]
 puts $report {<html><body><table border="1">}
 puts $report {<tr><td colspan = "10" > Static timing analysis - Post synthesis</td></tr>}
 puts $report {<tr><td title='Simulation Number'>SI No.</td><td title='Process/Voltage/Temperature Corner'>PVT Corner</td><td>Start/end point(setup)</td><td>Start/end point(hold)</td><td title='nanoseconds'>Setup slack</td><td title='nanoseconds'>Hold slack</td></tr>}
-set rpt [glob Post_Synthesis/*.rpt]
+set rpt [lsort [glob Post_Synthesis/*.rpt] ]
 set count 0
 foreach rpt_file $rpt {	
 	set rd_report [open $rpt_file r]
@@ -32,7 +32,7 @@ foreach rpt_file $rpt {
 	set end1 $end(1)
 	set sla0 $sla(0)
 	set sla1 $sla(1)
-        puts $report "<td>$count</td><td>$filename</td><td>$start1/$end1</td><td>$start0/$end0</td><td>$sla1</td><td>$sla0</td></tr>"
+        puts $report "<td>$count</td><td><a href ='$rpt_file' target ='_blank'>$filename </a></td><td>$start0/$end0</td><td>$start1/$end1</td><td>$sla0</td><td>$sla1</td></tr>"
 	incr count
 }
 puts $report {</table></body></html>}
