643fc2437d3a77332daa28694cb65ebe1da9d701
Geetima Kachari <er.geetimakachari@gmail.com>
2022-08-28 19:30:52 +0530

    Update riscv_conf.tcl

--- resources/riscv_conf.tcl
+++ resources/riscv_conf.tcl
@@ -40,19 +40,20 @@ foreach lines $lib_dir {
 			}
 		set preroute_file_path [lindex $verilog_files 2]
 		if {[file exists $preroute_file_path]} {
-			set filename3 [string totitle [lindex [split [lindex [split [file rootname [file tail $preroute_file_path] ] .] 1] _] 1] ]
-			mkdir -p Post_$filename3
-			mkdir -p Post_$filename3\_WNS
-			mkdir -p Post_$filename3\_TNS
+			set filename3 [lindex [split [lindex [split [file rootname [file tail $preroute_file_path] ] .] 1] _] 1] 
+			set filename4 [string map {"preroute" "Layout"} $filename3]
+			mkdir -p Post_$filename4
+			mkdir -p Post_$filename4\_WNS
+			mkdir -p Post_$filename4\_TNS
 			read_verilog $preroute_file_path
 			link_design rvmyth	
 			read_sdc -echo rvmyth.sdc
 			set_propagated_clock [all_clocks]
 			read_spef rvmyth.spef
 			#report_checks -path_delay min_max -fields {nets cap slew input_pins} -format full_clock_expanded -digits {4} > Post_Preroute/$new_name\_Post_Preroute_minmax.rpt
-			report_checks -path_delay min_max -fields {nets cap slew input_pins} -digits {4} > Post_Preroute/$new_name\_Post_Preroute_minmax.rpt
-			report_wns > Post_Preroute_WNS/$new_name\_Post_preroute_wns.rpt
-			report_tns > Post_Preroute_TNS/$new_name\_Post_preroute_tns.rpt
+			report_checks -path_delay min_max -fields {nets cap slew input_pins} -digits {4} > Post_Layout/$new_name\_Post_layout_minmax.rpt
+			report_wns > Post_Layout_WNS/$new_name\_Post_layout_wns.rpt
+			report_tns > Post_Layout_TNS/$new_name\_Post_layout_tns.rpt
 			}
 	
 	
